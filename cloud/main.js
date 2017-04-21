2eb901c3cc9787b931a65942bf63696a535adc6c
Huallyd <huallyd@gmail.com>
2017-04-21 18:28:06 -0300

    set isDeleted false before salve Comment model.

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
 Parse.Cloud.define('testehuallyd', function (req, res) {
 
-    
+
     if (testehuallydfuncao("oi")) {
     res.success({msg:"foi"})
 
@@ -17,3 +17,27 @@ function testehuallydfuncao(variavelGAMBIRA) {
         return false
     }
 }
+
+Parse.Cloud.beforeSave("Comment", function(request, response) {
+  request.object.set("isDeleted", false);
+  response.success();
+});
+
+Parse.Cloud.define('editUser', function(request, response) {
+    var userId = request.params.userId;
+
+    var User = Parse.Object.extend('_User'),
+        user = new User({ objectId: userId });
+
+    var currentUser = request.user;
+
+    var relation = user.relation("friendsRelation");
+    relation.add(currentUser);
+
+    Parse.Cloud.useMasterKey();
+    user.save().then(function(user) {
+        response.success(user);
+    }, function(error) {
+        response.error(error)
+    });
+});

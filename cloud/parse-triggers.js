881a63deacc7536071aa13ecd56cd9c2afdff429
Huallyd <huallyd@gmail.com>
2017-09-17 18:14:52 -0300

    Average stars method

--- cloud/parse-triggers.js
+++ cloud/parse-triggers.js
@@ -367,22 +367,24 @@ Parse.Cloud.define("validatePromoCode", function (request, response) {
     });
 });
 
-// Parse.Cloud.define("averageStars", function(request, response) {
-//   var query = new Parse.Query("Evaluation");
-//   query.equalTo("movie", request.params.movie);
-//   query.find({
-//     success: function(results) {
-//       var sum = 0;
-//       for (var i = 0; i < results.length; ++i) {
-//         sum += results[i].get("stars");
-//       }
-//       response.success(sum / results.length);
-//     },
-//     error: function() {
-//       response.error("movie lookup failed");
-//     }
-//   });
-// });
+Parse.Cloud.define("averageStars", function(request, response) {
+  fetchProfile(request.params.profileId).then(function (profile) {
+      var query = new Parse.Query("Evaluation");
+      query.equalTo("evaluated", profile);
+      query.find({
+         success: function(results) {
+         var sum = 0;
+         for (var i = 0; i < results.length; ++i) {
+           sum += results[i].get("amountStars");
+         }
+         response.success(sum / results.length);
+     },
+    error: function(err) {
+      response.error(err);
+    }
+   });
+  });
+});
 
 
 
